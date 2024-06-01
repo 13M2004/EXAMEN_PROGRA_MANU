fe018a87d396fcfcc094e2b57345facc4a61f7f0
Alexander Monzon <mmonzonp4@miumg.edu.gt>
2024-05-31 18:15:46 -0600

    Agregar archivos de proyecto.

---
+++ Examen_Progra_Manuel/Copa_Mundo.cs
@@ -0,0 +1,177 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFA_WORLD_CUP
+{
+    internal class Copa_Mundo()
+    {
+        // Información de conexión a la base de datos
+        private string connectionString = "Server = localhost; Database = db_universidad; Uid=root;Pwd=alex123";
+
+
+        //constructor
+        public Copa_Mundo(string servidor, string usur, string pwd)
+        {
+            connectionString = "Server=" + servidor + ";Database=db_universidad;Uid=" + usur + ";Pwd=" + pwd + "alex123";
+        }
+
+        // Método para leer todos los personajes
+        public DataTable LeerSelecciones()
+        {
+            DataTable personajes = new DataTable();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM db_universidad.mundial2018";
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        adapter.Fill(personajes);
+                    }
+                }
+            }
+
+            return personajes;
+        }
+
+        public int CrearSleccion(int Id_Selecciones, string Nombre_Selecciones, string Jugadores_Destacados, int No_Clasificaciones_Mundial, string Frases_Selecciones, DateTime Fecha_Ultimo_Mundial_Ganado, int Valor_Plantilla)
+        {
+            // Validar los datos antes de la inserción
+            if (Id_Selecciones <= 0 || string.IsNullOrWhiteSpace(Nombre_Selecciones) ||
+            string.IsNullOrWhiteSpace(Jugadores_Destacados) || No_Clasificaciones_Mundial <= 0 ||
+            string.IsNullOrWhiteSpace(Frases_Selecciones) || Fecha_Ultimo_Mundial_Ganado == null ||
+            Valor_Plantilla <= 0 || string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("Uno o más parámetros son inválidos.");
+            }
+
+
+
+            // Intentar la inserción y manejar posibles errores
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO mundial2018 (Id_Selecciones, Nombre_Selecciones, Jugadores_Destacados, No_Clasificaciones_Mundial, Frases_Selecciones, Fecha_Ultimo_Mundial_Ganado, Valor_Plantilla) VALUES (@Id_Selecciones, @Nombre_Selecciones, @Jugadores_Destacados, @No_Clasificaciones_Mundial, @Frases_Selecciones, @Fecha_Ultimo_Mundial_Ganado, @Valor_Plantilla)";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id_Selecciones", Id_Selecciones);
+                        command.Parameters.AddWithValue("@Nombre_Selecciones", Nombre_Selecciones);
+                        command.Parameters.AddWithValue("@Jugadores_Destacados", Jugadores_Destacados);
+                        command.Parameters.AddWithValue("@No_Clasificaciones_Mundial", No_Clasificaciones_Mundial);
+                        command.Parameters.AddWithValue("@Frases_Selecciones", Frases_Selecciones);
+                        command.Parameters.AddWithValue("@Fecha_Ultimo_Mundial_Ganado", Fecha_Ultimo_Mundial_Ganado);
+                        command.Parameters.AddWithValue("@Valor_Plantilla", Valor_Plantilla);
+
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                // Manejar errores específicos de MySQL
+                throw new Exception("Error de MySQL: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                // Manejar otros errores
+                throw new Exception("Error: " + ex.Message);
+            }
+        }
+
+        public int ActualizarSelecciones(int Id_Selecciones, string Nombre_Selecciones, string Jugadores_Destacados, int No_Clasificaciones_Mundial, string Frases_Selecciones, DateTime Fecha_Ultimo_Mundial_Ganado, int Valor_Plantilla)
+        {
+            // Validar los datos antes de la inserción
+            if (Id_Selecciones <= 0 || string.IsNullOrWhiteSpace(Nombre_Selecciones) ||
+            string.IsNullOrWhiteSpace(Jugadores_Destacados) || No_Clasificaciones_Mundial <= 0 ||
+            string.IsNullOrWhiteSpace(Frases_Selecciones) || Fecha_Ultimo_Mundial_Ganado == null ||
+            Valor_Plantilla <= 0 || string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("Uno o más parámetros son inválidos.");
+            }
+
+            //ACTUALIZACION DE SELECCIONES
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "UPDATE db_universidad.mundial2018 SET Nombre_Selecciones = @Nombre_Selecciones, Jugadores_Destacados = @Jugadores_Destacados, No_Clasificaciones_Mundial = @No_Clasificaciones_Mundial, Frases_Selecciones = @Frases_Selecciones, Fecha_Ultimo_Mundial_Ganado = @Fecha_Ultimo_Mundial_Ganado, Valor_Plantilla = @Valor_Plantilla WHERE Id_Selecciones = @Id_Selecciones;";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id_Selecciones", Id_Selecciones);
+                        command.Parameters.AddWithValue("@Nombre_Selecciones", Nombre_Selecciones);
+                        command.Parameters.AddWithValue("@Jugadores_Destacados", Jugadores_Destacados);
+                        command.Parameters.AddWithValue("@No_Clasificaciones_Mundial", No_Clasificaciones_Mundial);
+                        command.Parameters.AddWithValue("@Frases_Selecciones", Frases_Selecciones);
+                        command.Parameters.AddWithValue("@Fecha_Ultimo_Mundial_Ganado", Fecha_Ultimo_Mundial_Ganado);
+                        command.Parameters.AddWithValue("@Valor_Plantilla", Valor_Plantilla);
+
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                // Manejar errores específicos de MySQL
+                Console.WriteLine("Error de MySQL: " + ex.Message);
+                return -1; // O cualquier otro valor que indique un error
+            }
+            catch (Exception ex)
+            {
+                // Manejar otros errores
+                Console.WriteLine("Error: " + ex.Message);
+                return -1; // O cualquier otro valor que indique un error
+            }
+
+
+        }
+
+        //ELIMINAR SELECCION
+        public int EliminarSeleccion(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ID inválido.");
+            }
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "DELETE FROM db_universidad.mundial2018 WHERE Id_Selecciones = @Id_Selecciones;";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Id_Selecciones", Id_Selecciones);
+                        return command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error de MySQL: " + ex.Message);
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return -1;
+            }
+        }
+
+
+    }
+}
---
+++ Examen_Progra_Manuel/Examen_Progra_Manuel/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Examen_Progra_Manuel
+{
+    public partial class Form1 : Form
+    {
+     
+        private string[] Nombre_Selecciones =
+        {
+            "Qatar",
+            "Ecuador",
+            "Senegal",
+            "Paises Bajos",
+            "Inglaterra",
+            "Iran",
+            "Estados Unidos",
+            "Gales",
+            "Argentina",
+            "Arabia Saudita",
+            "Mexico",
+            "Polonia",
+            "Francia",
+            "Australia",
+            "Dinamarca",
+            "Tunez",
+            "España",
+            "Costa Rica",
+            "Alemania",
+            "Japon",
+            "Belgica",
+            "Canada",
+            "Marruecos",
+            "Croacia",
+            "Brasil",
+            "Serbia",
+            "Suiza",
+            "Camerun",
+            "Portugal",
+            "Ghana",
+            "Uruguay",
+            "Corea del Sur"
+        };
+
+      
+        private Mundial seleccion;
+
+        private void CargarSlecciones()
+        {
+            dataGridViewMundial.DataSource = seleccion.LeerSelecciones();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            seleccion = new Mundial ("localhost", "root", "");
+            this.Load += new System.EventHandler(this.Form1_Load);
+        }
+
+        // MOSTRAR LSITADO
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxSelecciones.Items.AddRange(Nombre_Selecciones);
+        }
+
+
+        //MOSTRAR PERSONAJES
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+            dataGridViewMundial.DataSource = seleccion.LeerSelecciones();
+        }
+
+
+        private void comboBoxGrupo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBoxSelecciones.Items.AddRange(Nombre_Selecciones);
+        }
+
+
+        //AGREGAR PERSONAJE
+        private void buttonInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtener valores de los controles de la interfaz de usuario               
+                string nombre_selecciones = comboBoxSelecciones.Text;
+                string Jugadores_Destacados = textBoxJugadores_Destacados.Text;
+                string no_clasificaciones_mundial = textBoxNo_Clasificaciones_Mundial.Text;
+                string frases_selecciones = textBoxFrases_Selecciones.Text;
+                string fecha_ultimo_mundial_ganado = dateTimePickerFecha_Ultimo_Mundial_Ganado.Text;
+                int valor_plantilla = (int)numericUpDownValor_Plantilla.Value;
+           
+
+                // Llamar al método CrearSeleccion y obtener la respuesta
+                int respuesta = seleccion.CrearPersonaje(nombre_selecciones, Jugadores_Destacados, no_clasificaciones_mundial, frases_selecciones, fecha_ultimo_mundial_ganado, valor_plantilla);
+
+                // Verificar la respuesta e informar al usuario
+                if (respuesta > 0)
+                {
+                    MessageBox.Show("Personaje creado correctamente");
+                    dataGridViewMundial.DataSource = seleccion.LeerPersonajes();
+                }
+                else
+                {
+                    MessageBox.Show("Error al crear el personaje");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción y mostrar un mensaje de error al usuario
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+        }
+
+
+        //ACTUALIZAR
+        private void buttonActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Verificar que haya un registro seleccionado en el DataGridView
+                if (dataGridViewMundial.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Seleccione un personaje para actualizar.");
+                    return;
+                }
+
+                // Obtener la fila seleccionada
+                DataGridViewRow row = dataGridViewMundial.SelectedRows[0];
+
+                // Obtener el ID del personaje (asumiendo que hay una columna "id")
+                int id = Convert.ToInt32(row.Cells["id"].Value);
+
+                // Obtener los valores de los controles de la interfaz de usuario
+                string nombre_selecciones = comboBoxSelecciones.Text;
+                string Jugadores_Destacados = textBoxJugadores_Destacados.Text;
+                string no_clasificaciones_mundial = textBoxNo_Clasificaciones_Mundial.Text;
+                string frases_selecciones = textBoxFrases_Selecciones.Text;
+                string fecha_ultimo_mundial_ganado = dateTimePickerFecha_Ultimo_Mundial_Ganado.Text;
+                int valor_plantilla = (int)numericUpDownValor_Plantilla.Value;
+
+                // Validar los datos antes de la actualización
+                if (string.IsNullOrWhiteSpace(nombre_selecciones) || string.IsNullOrWhiteSpace(Jugadores_Destacados) ||
+                string.IsNullOrWhiteSpace(no_clasificaciones_mundial) || string.IsNullOrWhiteSpace(frases_selecciones) ||
+                string.IsNullOrWhiteSpace(fecha_ultimo_mundial_ganado))
+                {
+                    MessageBox.Show("Uno o más parámetros son inválidos.");
+                    return;
+                }
+
+                // Confirmar con el usuario si desea realizar la actualización
+                DialogResult result = MessageBox.Show("¿Está seguro de que desea actualizar este personaje?", "Confirmar Actualización", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    // Llamar al método para actualizar el personaje
+                    int respuesta = seleccion.ActualizarPersonaje(id, nombre_selecciones, Jugadores_Destacados, no_clasificaciones_mundial, frases_selecciones, fecha_ultimo_mundial_ganado, valor_plantilla);
+
+                    // Verificar la respuesta del método de actualización
+                    if (respuesta > 0)
+                    {
+                        MessageBox.Show("Personaje actualizado correctamente.");
+                        // Actualizar el DataGridView para reflejar los cambios
+                        dataGridViewMundial.DataSource = seleccion.LeerPersonajes();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al actualizar el personaje.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier excepción y mostrar un mensaje de error al usuario
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+
+
+        }
+
+
+        //ELIMINAR SELECCCION
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridViewMundial.SelectedRows.Count == 0)
+                {
+                    MessageBox.Show("Seleccione un personaje para eliminar.");
+                    return;
+                }
+
+                DataGridViewRow row = dataGridViewMundial.SelectedRows[0];
+                int id = Convert.ToInt32(row.Cells["id"].Value);
+
+                var confirmResult = MessageBox.Show("¿Está seguro de que desea eliminar esta SELECCIÓN?",
+                                                     "Confirmar Eliminación",
+                                                     MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    int respuesta = seleccion.EliminarPersonaje(id);
+
+                    if (respuesta > 0)
+                    {
+                        MessageBox.Show("Personaje eliminado correctamente");
+                        CargarSlecciones();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al eliminar el personaje");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+        }
+
+
+    }
+}
